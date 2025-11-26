b11da3f12037aadc906536e478eba076bbd62dac
lukas-sgx <lukas.soigneux@epitech.eu>
2025-11-26 10:34:40 +0100

    [+] Add syscall exit ctrl+c & syscall exec cmd

--- shell.asm
+++ shell.asm
@@ -3,10 +3,12 @@ bits 64
 section .data
     msg db "sh-1.0$ "
     len equ $-msg
+    path db "/bin/ls", 0
+    argv dq path, 0
 
 section .bss
     buf resb 128
-    
+
 section .text
     global _start
     _start:
@@ -22,9 +24,42 @@ section .text
         mov rdx, 128
         syscall
 
-        mov rax, 1
-        mov rdi, 1
-        lea rsi, [rel buf]
+        cmp rax, 0
+        je _exit
+
+        mov rax, 57
         syscall
-    
-    jmp _start
+
+        cmp rax, 0
+        je _exec
+        jl _fork_error
+
+        mov rdi, rax
+        xor     rsi, rsi
+        xor     rdx, rdx
+        xor     r10, r10
+        mov     rax, 61
+        syscall
+
+        jmp _start
+
+    _exec:
+        mov rax, 59
+        lea rdi, [rel path]
+        lea rsi, [rel argv]
+        xor rdx, rdx
+        syscall
+        
+        mov rax, 60
+        mov rdi, 1
+        syscall
+
+    _exit:
+        mov rax, 60
+        mov rdi, 0
+        syscall
+
+    _fork_error:
+        mov     rax, 60
+        mov     rdi, 1
+        syscall
