9dabebe990176e86883ded9d0d654a30f01db0ae
lukas-sgx <lukas.soigneux@epitech.eu>
2025-11-26 12:21:00 +0100

    [+] Add loop & found bin

--- shell.asm
+++ shell.asm
@@ -3,63 +3,105 @@ bits 64
 section .data
     msg db "sh-1.0$ "
     len equ $-msg
-    path db "/bin/ls", 0
-    argv dq path, 0
+
+    not_found db "Command not found.", 10
+    len_notfound equ $-not_found
 
 section .bss
     buf resb 128
+    cmd_path resb 256
+    argv resq 2
 
 section .text
     global _start
-    _start:
-        mov rax, 1
-        mov rdi, 1
-        lea  rsi, [rel msg]
-        mov rdx, len
-        syscall
+_start:
+    mov rax, 1
+    mov rdi, 1
+    lea  rsi, [rel msg]
+    mov rdx, len
+    syscall
 
-        mov rax, 0
-        mov rdi, 0
-        lea rsi, [rel buf]
-        mov rdx, 128
-        syscall
+    mov rax, 0
+    mov rdi, 0
+    lea rsi, [rel buf]
+    mov rdx, 128
+    syscall
 
-        cmp rax, 0
-        je _exit
+    cmp rax, 0
+    je _exit
 
-        mov rax, 57
-        syscall
+    mov rcx, rax
+    mov rbx, cmd_path
 
-        cmp rax, 0
-        je _exec
-        jl _fork_error
+    mov byte [rbx], '/'
+    mov byte [rbx + 1], 'b'
+    mov byte [rbx + 2], 'i'
+    mov byte [rbx + 3], 'n'
+    mov byte [rbx + 4], '/'
+    lea rdi, [rel buf]
+    lea rbx, [rbx + 5]
 
-        mov rdi, rax
-        xor     rsi, rsi
-        xor     rdx, rdx
-        xor     r10, r10
-        mov     rax, 61
-        syscall
+    jmp copy_loop
 
-        jmp _start
+copy_loop:
+    cmp rcx, 0
+    je  _done_exec
+    mov dl, [rdi]
+    cmp dl, ' '
+    je _done_exec
+    cmp dl, 10
+    je _done_exec
+    mov [rbx], dl
+    inc rbx
+    inc rdi
+    dec rcx
+    jmp copy_loop
 
-    _exec:
-        mov rax, 59
-        lea rdi, [rel path]
-        lea rsi, [rel argv]
-        xor rdx, rdx
-        syscall
-        
-        mov rax, 60
-        mov rdi, 1
-        syscall
+_done_exec:
+    mov byte [rbx], 0
+    lea rax, [rel cmd_path]
+    mov [rel argv], rax
+    mov qword [rel argv + 8], 0
 
-    _exit:
-        mov rax, 60
-        mov rdi, 0
-        syscall
+    mov rax, 57
+    syscall
 
-    _fork_error:
-        mov     rax, 60
-        mov     rdi, 1
-        syscall
+    cmp rax, 0
+    je _exec
+    jl _fork_error
+
+    mov rdi, rax
+    xor rsi, rsi
+    xor rdx, rdx
+    xor r10, r10
+    mov rax, 61
+    syscall
+
+    jmp _start
+
+_exec:
+    mov rax, 59
+    lea rdi, [rel cmd_path]
+    lea rsi, [rel argv]
+    xor rdx, rdx
+    syscall
+    
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [rel not_found]
+    mov rdx, len_notfound
+    syscall
+
+    mov rax, 60
+    mov rdi, 1
+    syscall
+
+_exit:
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+_fork_error:
+    mov     rax, 60
+    mov     rdi, 1
+    syscall
